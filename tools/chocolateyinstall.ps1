ab9192ad0b2755abcb0d29a34c58425d2435bb52
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2019-07-08 14:19:00 +0200

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'purplepen-323.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file         = $fileLocation
+
+  softwareName  = 'Purple Pen*'
+  checksum      = '5EFB88501DFE89B9258FBBB6C836A880A0E2E2621AA2975EC471A94BA93E0BB1'
+  checksumType  = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
